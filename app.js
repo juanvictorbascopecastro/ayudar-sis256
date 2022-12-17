d643ce217f081fcf8e15aecb7317982ae95bfd03
Juan Victor Bascope Castro <juan.victor.bascope.castro@gmail.com>
2022-12-17 08:48:38 -0400

    pregunta 2

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
+function pregunta2(text) {
+    document.getElementById('contenido').innerHTML = 'modo blanco y negro'
+    document.getElementById('titulo').style.setProperty('background-color', 'var(--gray-contenido)');
+    document.getElementById('titulo').style.setProperty('border', '2px solid var(--gray-border)');
+    document.getElementById('resultado').style.setProperty('background-color', 'var(--gray-contenido)');
+    document.getElementById('resultado').style.setProperty('border', '2px solid var(--gray-border)');
+    document.getElementById('menu').style.setProperty('border', '2px solid var(--gray-border)')
+    document.getElementById('mensaje').style.setProperty('border', '2px solid var(--gray-border)')
+    document.getElementById('mensaje').style.setProperty('background-color', 'var(--gray-contenido)');
+    setTitulo(text)
+}
 function setTitulo(text) {
     document.getElementById('titulo').innerHTML = text
 }
