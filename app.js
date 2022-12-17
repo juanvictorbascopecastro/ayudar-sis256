ca27221d6ef480cb185427e2dc293b96c4e6e7fc
Juan Victor Bascope Castro <juan.victor.bascope.castro@gmail.com>
2022-12-17 08:26:11 -0400

    pregunta 2

--- app.js
+++ app.js
@@ -1,29 +1,3 @@
-function pagina() {
-    fetch('paginas/datos.html')
-    .then(response => response.text())
-    .then(data => {
-        document.getElementById("contenido").innerHTML = data;
-    })
-    .catch(error => console.log(error));
-}
 function setTitulo(text) {
     document.getElementById('titulo').innerHTML = text
-}
-function pagina2() {
-    var ajax=new XMLHttpRequest();
-    ajax.onreadystatechange=function(){
-        if(ajax.readyState==4){
-            document.getElementById("contenido").innerHTML=ajax.responseText;
-        }
-    }
-    ajax.open("GET",'paginas/pagina2.html',true);
-    ajax.send();
-}
-
-    //  parrafos[i].style.display = "block";
-    // parrafos[i].style.display = "none";
-
-function enviar() {
-    const form = document.getElementById('form-msg');
-    console.log(form.nombre.value)
 }
